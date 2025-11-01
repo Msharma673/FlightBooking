e23cae67bcf57d5c866f8d563b1cb244e2414991
Deependra Kumar Rout <53940680+Deependrakumarrout@users.noreply.github.com>
2025-11-01 21:49:17 +0530

    Initial commit

---
+++ src/main/java/com/flight_reservation/FlightReserve/FlightReserveApplication.java
@@ -0,0 +1,13 @@
+package com.flight_reservation.FlightReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReserveApplication.class, args);
+	}
+
+}
