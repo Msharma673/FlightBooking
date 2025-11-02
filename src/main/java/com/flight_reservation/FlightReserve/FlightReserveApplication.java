cb807732175cd0f9fde5a9ff90720e0f7115991a
Deependra Kumar Rout <53940680+Deependrakumarrout@users.noreply.github.com>
2025-11-02 23:18:42 +0530

    PROJECT STRUCTURE FR

--- src/main/java/com/flight_reservation/FlightReserve/FlightReserveApplication.java
+++ src/main/java/com/flight_reservation/FlightReserve/FlightReserveApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightReserveApplication {
-
+	// whole final project structure
 	public static void main(String[] args) {
 		SpringApplication.run(FlightReserveApplication.class, args);
 	}
